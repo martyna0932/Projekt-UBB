f936e89539d6f970725ec1e288ebc1d0baa77ed7
Martyna <martyna263@onet.eu>
2025-01-06 15:15:26 +0100

    poprawki nazw plików

--- ProjektIO/connect.php
+++ ProjektIO/connect.php
@@ -1,13 +1,35 @@
 <?php
-$host = 'localhost';
-$db = 'nazwa_bazy';
-$user = 'uzytkownik';
-$password = 'haslo';
+// Połączenie z bazą danych
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "polskiekino";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Błąd połączenia: " . $e->getMessage());
+// Tworzymy połączenie
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Sprawdzamy połączenie
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Zapytanie do bazy danych, aby pobrać plakaty
+$sql = "SELECT * FROM posters";
+$result = $conn->query($sql);
+
+// Tworzymy tablicę, aby przechować wyniki
+$posters = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $posters[] = $row;
+    }
+}
+
+// Zamykamy połączenie
+$conn->close();
+
+// Wysyłamy dane w formacie JSON
+header('Content-Type: application/json');
+echo json_encode($posters);
 ?>
